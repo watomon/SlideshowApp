396b1c3a8338f914f4837c82ea2fa5c88e144324
watomon <watomon1@gmail.com>
2018-02-04 21:18:33 +0900

    create a new project

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   weak var images: UIImageView = ["01.png", "02.png", "03.png"]
+    
+    @IBOutlet weak var dispImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        dispImage = images[0]
     }
 
     override func didReceiveMemoryWarning() {
