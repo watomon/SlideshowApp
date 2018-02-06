d6a3b4cf3215b78d90fecc2af411fa90fa6d0788
watomon <watomon1@gmail.com>
2018-02-06 23:51:48 +0900

    add buttons

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -15,14 +15,24 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myImageView: UIImageView!
     
+    @IBAction func nextImage(_ sender: Any) {
+    }
+    
+    @IBAction func preImage(_ sender: Any) {
+    }
+    
+    @IBAction func slidshoeImage(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
             
         //myImageView = UIImageView(frame: CGRectMake(0, 0, 100, 120));
         
+        var imageNum = 0
         //表示する画像を設定する
-        let myImage = UIImage(named: images[0])
+        let myImage = UIImage(named: images[imageNum])
         
         //画像をUIImageViewに設定する
         myImageView.image = myImage
@@ -30,6 +40,7 @@ class ViewController: UIViewController {
         //画像の位置を設定する
         //myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y: 200.0)
         
+        //画像を表示させる
         self.view.addSubview(myImageView)
         
     }
